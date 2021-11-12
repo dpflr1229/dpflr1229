d8ccb98e23e25c3377d3cb0c96642061458f3668
dpflr1229 <81514835+dpflr1229@users.noreply.github.com>
2021-11-12 23:28:37 +0900

    Add files via upload

---
+++ 텍스트분석/텍스트분석.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+get_ipython().system('pip install jupytext ')
+get_ipython().system('jupyter notebook --generate-config')
+
+
+# In[ ]:
+
+
+
+
